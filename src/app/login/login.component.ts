da3e4eec7dc267cd0306f1b16c723d403dcfa290
nourdine <kacemnourdine@gmail.com>
2018-09-21 14:11:10 +0100

    register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router';
+import {MatDialog} from '@angular/material';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+username: string;
+password: string;
 
   ngOnInit() {
   }
+login(): void {
+    if (this.username === 'admin' && this.password === 'admin') {
+     this.router.navigate(['popUp']);
+    }
+    else{
+      alert('Invalid credentials');
+  }
+  }
+  }
 
-}
